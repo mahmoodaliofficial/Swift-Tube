6fa795194b86095314416e8025b92439f0b7f145
Mahmood Ali <127453068+mahmoodaliofficial@users.noreply.github.com>
2026-07-31 03:00:58 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt-get update
-apt-get install -y ffmpeg
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker backend.main:app
+apt-get update && apt-get install -y ffmpeg
+pip install -r requirements.txt
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker backend.main:app --bind 0.0.0.0:8000
